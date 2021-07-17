2c512d4f196fa0c5408d37606cc947af9ade88f4
AJ <liaofuyou@yeah.net>
2021-07-17 12:26:49 +0800

    motify

--- lenet.py
+++ lenet.py
@@ -1,22 +1,11 @@
-import pytorch_lightning as pl
-import torch
-import torchmetrics
 from torch import nn
-from torch.nn import Sequential
-from torch.nn import functional as F
-
-from mnist_datamodule import MNISTDataModule
+from torch.nn import Sequential, Module
 
 
-class LitClassifier(pl.LightningModule):
+class LeNet(Module):
 
-    def __init__(
-            self,
-            hidden_dim: int = 128,
-            learning_rate: float = 0.0001,
-    ):
+    def __init__(self):
         super().__init__()
-        self.save_hyperparameters()
         self.lenet = Sequential(nn.Conv2d(1, 6, kernel_size=(5, 5), padding=2),
                                 nn.Sigmoid(),
                                 nn.AvgPool2d(kernel_size=2, stride=2),
@@ -30,51 +19,5 @@ class LitClassifier(pl.LightningModule):
                                 nn.Sigmoid(),
                                 nn.Linear(84, 10))
 
-        self.metric = torchmetrics.Accuracy()
-
     def forward(self, x):
         return self.lenet(x)
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self(x)
-        loss = F.cross_entropy(y_hat, y)
-
-        # Accuracy
-        print(f"Accuracy on batch {batch_idx}: {self.metric(y_hat, y)}")
-        return loss
-
-    def on_train_batch_end(self, outputs, batch, batch_idx: int, dataloader_idx: int) -> None:
-        # Accuracy
-        print(f"Accuracy on all data: {self.metric.compute()}")
-
-    def validation_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self(x)
-        loss = F.cross_entropy(y_hat, y)
-        self.log('valid_loss', loss)
-
-    def test_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self(x)
-        loss = F.cross_entropy(y_hat, y)
-        self.log('test_loss', loss)
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
-
-
-def cli_main():
-    # init model
-    model = LitClassifier()
-
-    # init data
-    dm = MNISTDataModule()
-
-    # train
-    trainer = pl.Trainer(val_check_interval=0.25)
-    trainer.fit(model, dm)
-
-
-if __name__ == '__main__':
-    cli_main()
---
+++ model.py
@@ -0,0 +1,77 @@
+import pytorch_lightning as pl
+import torch
+import torchmetrics
+from torch import nn
+from torch.nn import Sequential
+from torch.nn import functional as F
+
+from lenet import LeNet
+from mnist_datamodule import MNISTDataModule
+
+
+class ImageClassifier(pl.LightningModule):
+
+    def __init__(
+            self,
+            net_name="lenet",
+            hidden_dim: int = 128,
+            learning_rate: float = 0.0001,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+
+        if net_name == "lenet":
+            self.net = LeNet()
+        elif net_name == "alexnet":
+            self.net = LeNet()
+        else:
+            self.net = LeNet()
+
+        self.metric = torchmetrics.Accuracy()
+
+    def forward(self, x):
+        return self.net(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+
+        # Accuracy
+        print(f"Accuracy on batch {batch_idx}: {self.metric(y_hat, y)}")
+        return loss
+
+    def on_train_batch_end(self, outputs, batch, batch_idx: int, dataloader_idx: int) -> None:
+        # Accuracy
+        print(f"Accuracy on all data: {self.metric.compute()}")
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log('valid_loss', loss)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log('test_loss', loss)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+
+
+def cli_main():
+    # init model
+    model = ImageClassifier("lenet")
+
+    # init data
+    dm = MNISTDataModule()
+
+    # train
+    trainer = pl.Trainer(val_check_interval=0.25)
+    trainer.fit(model, dm)
+
+
+if __name__ == '__main__':
+    cli_main()
